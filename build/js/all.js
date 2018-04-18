24392a0bb4c8d8ca0142a0429875c365b2b6d33a
nemerki <mmehemmedm@gmail.com>
2018-04-18 11:42:01 +0400

    menu ve isleric

--- build/js/all.js
+++ build/js/all.js
@@ -51,3 +51,35 @@ $(function(){
 		}
 	});
 });
+$(document).ready(function() {
+
+  var scrollLink = $('.scroll');
+  
+  // Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+  
+
+
+
+  $(window).scroll(function() {
+   if( $(this).scrollTop() > 50 ) {
+    $(".logo").removeClass("img-fluid");
+    $(".navbar").addClass("fixed-top");
+    $(".navbar").addClass("fixed-top--size");
+    $(".header2 .navbar").addClass("fixed-top--dark");
+  } else {
+
+    $(".navbar").addClass("sticky-top");
+    $(".navbar").removeClass("fixed-top--size");
+    $(".logo").addClass("img-fluid");
+    $(".header2 .navbar").removeClass("fixed-top--dark");
+  }
+});
+
+  
+})
